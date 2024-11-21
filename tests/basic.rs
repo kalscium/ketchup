024f8d726ec4a72f92ff8ac6ae04198e299cec0a
kalscium <kalscium@protonmail.com>
2024-11-21 11:22:07 +1100

    unary recursive tests: added more tests

--- tests/basic.rs
+++ tests/basic.rs
@@ -9,14 +9,14 @@ enum Node {
     Div,
     Neg,
     Pos,
-    Call,
+    Call(i32),
 }
 
 impl node::Node for Node {
     fn get_kind(&self) -> NodeKind {
         match self {
             Node::Number(_) => NodeKind::Operand,
-            Node::Call => NodeKind::Unary,
+            Node::Call(_) => NodeKind::Unary,
             Node::Pos => NodeKind::Unary,
             Node::Neg => NodeKind::Unary,
             Node::Add => NodeKind::Binary,
@@ -29,7 +29,7 @@ impl node::Node for Node {
     fn get_precedence(&self) -> Precedence {
         match self {
             Node::Number(_) => Precedence::MAX,
-            Node::Call => Precedence::MAX-1,
+            Node::Call(_) => Precedence::MAX-1,
             Node::Pos => Precedence::MAX-2,
             Node::Neg => Precedence::MAX-2,
             Node::Mul => 1,
@@ -86,8 +86,32 @@ fn unary_right_align() {
     parse::unary_left_align(Node::Pos, &mut asa).unwrap();
     parse::unary_left_align(Node::Neg, &mut asa).unwrap();
     parse::operand(Node::Number(1), &mut asa).unwrap();
-    parse::unary_right_align(Node::Call, true, &mut asa).unwrap();
+    parse::unary_right_align(Node::Call(2), true, &mut asa).unwrap();
 
     assert!(*asa.completed());
-    assert_eq!(asa.vector[..], [Node::Neg, Node::Pos, Node::Neg, Node::Call, Node::Number(1)]);
+    assert_eq!(asa.vector[..], [Node::Neg, Node::Pos, Node::Neg, Node::Call(2), Node::Number(1)]);
+}
+
+#[test]
+fn unary_right_align_left_recursive() {
+    let mut asa = VectorASA::<Node>::new();
+    parse::operand(Node::Number(1), &mut asa).unwrap();
+    parse::unary_right_align(Node::Call(2), true, &mut asa).unwrap();
+    parse::unary_right_align(Node::Call(3), true, &mut asa).unwrap();
+    parse::unary_right_align(Node::Call(4), true, &mut asa).unwrap();
+
+    assert!(*asa.completed());
+    assert_eq!(asa.vector[..], [Node::Call(4), Node::Call(3), Node::Call(2), Node::Number(1)]);
+}
+
+#[test]
+fn unary_right_align_right_recursive() {
+    let mut asa = VectorASA::<Node>::new();
+    parse::operand(Node::Number(1), &mut asa).unwrap();
+    parse::unary_right_align(Node::Call(2), false, &mut asa).unwrap();
+    parse::unary_right_align(Node::Call(3), false, &mut asa).unwrap();
+    parse::unary_right_align(Node::Call(4), false, &mut asa).unwrap();
+
+    assert!(*asa.completed());
+    assert_eq!(asa.vector[..], [Node::Call(2), Node::Call(3), Node::Call(4), Node::Number(1)]);
 }
